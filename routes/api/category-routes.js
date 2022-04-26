68aa330a4341f235619e0383d62a0e11889dc786
ElmerPC <erey2790@gmail.com>
2022-04-26 02:02:53 -0500

    findOne Category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,6 +29,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id']
+    }
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.post('/', (req, res) => {
